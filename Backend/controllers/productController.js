d86448adc0fde2babf0116c3d1fbc18ccc917dac
nasim67reja <nasimreja67@gmail.com>
2022-07-02 15:41:48 -0700

    Updated

--- Backend/controllers/productController.js
+++ Backend/controllers/productController.js
@@ -4,15 +4,39 @@ exports.getAllProducts = async (req, res) => {
   try {
     // 1 (a) . Filtering
     const { page, sort, limit, fields, ...queryObj } = req.query;
-
+    console.log(page, sort, limit, fields);
     // 1 (b) . Advanced Filtering
     let queryStr = JSON.stringify(queryObj);
 
     queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
 
-    let query = await Product.find(JSON.parse(queryStr));
+    let query = Product.find(JSON.parse(queryStr));
 
-    // const allProduct = await Product.find();
+    // 2. Sorting
+
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(',').join(' ');
+      query.sort(sortBy);
+    } else query.sort('ratingsAverage');
+
+    // 3. Field Limiting
+    if (req.query.fields) {
+      const Fields = req.query.fields.split(',').join(' ');
+      query.select(Fields);
+    } else query.select('-__v');
+
+    //  4 Pagination
+    const Page = req.query.page * 1 || 1;
+    const Limit = req.query.limit * 1 || 100;
+    const skip = Limit * (Page - 1);
+    query = await query.skip(skip).limit(Limit);
+
+    if (req.query.page) {
+      const numTours = await Product.countDocuments();
+      console.log(skip, numTours);
+      if (skip >= numTours) throw new Error('This page does not exist');
+    }
+
     res.status(200).json({
       status: 'success',
       results: query.length,
@@ -23,7 +47,7 @@ exports.getAllProducts = async (req, res) => {
   } catch (err) {
     res.status(404).json({
       status: 'fail',
-      message: 'invalid',
+      message: err.message,
     });
   }
 };
