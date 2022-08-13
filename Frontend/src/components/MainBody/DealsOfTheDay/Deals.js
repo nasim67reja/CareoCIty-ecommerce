e4acfd545d9de008d1cb64cf2a746d13e75c6116
nasim67reja <nasimreja67@gmail.com>
2022-08-13 03:47:03 -0700

    Completed Slider Product

--- Frontend/src/components/MainBody/DealsOfTheDay/Deals.js
+++ Frontend/src/components/MainBody/DealsOfTheDay/Deals.js
@@ -1,121 +1,16 @@
-// import React from "react";
-import { useEffect, useRef, useState } from "react";
-import img1 from "../../../assets/jacket1.webp";
-import img2 from "../../../assets/jacket2.webp";
-import lip1 from "../../../assets/ltp-1.webp";
-import lip2 from "../../../assets/lip-2.webp";
-import head1 from "../../../assets/head-1.webp";
-import head2 from "../../../assets/head-2.webp";
-import women1 from "../../../assets/women1.webp";
-import women2 from "../../../assets/women2.webp";
-
-const deal = [
-  { img: img1, imgH: img2, title: "Winter Brand New Casual", price: 30.2 },
-  { img: lip1, imgH: lip2, title: "Winter Brand New Casual", price: 30.2 },
-  { img: head1, imgH: head2, title: "Winter Brand New Casual", price: 30.2 },
-  { img: women1, imgH: women2, title: "Winter Brand New Casual", price: 30.2 },
-  { img: img1, imgH: img2, title: "Winter Brand New Casual", price: 30.2 },
-  { img: lip1, imgH: lip2, title: "Winter Brand New Casual", price: 30.2 },
-  { img: img1, imgH: img2, title: "Winter Brand New Casual", price: 30.2 },
-  { img: lip1, imgH: lip2, title: "Winter Brand New Casual", price: 30.2 },
-  { img: img1, imgH: img2, title: "Winter Brand New Casual", price: 30.2 },
-];
-
-const SingleProduct = ({ product, i, numOfClick }) => {
-  const [width, setWidth] = useState(0);
-  const [mouseEnter, setMouseEnter] = useState(false);
-  const [clicked, setClicked] = useState(false);
-  const element = useRef(null);
-
-  useEffect(() => {
-    setWidth(element.current.offsetWidth + 20);
-  }, []);
-
-  let exactWidth = width * i + width * numOfClick;
-
-  return (
-    <div
-      style={{ transform: `translateX(${exactWidth}px)` }}
-      className="testclass absolute top-0 left-0 h-full transition-all duration-500"
-    >
-      <div
-        className="relative flex h-full w-full  flex-col  items-center gap-1 "
-        ref={element}
-      >
-        <div
-          className="sliderIcon absolute top-0 right-0 cursor-pointer"
-          onClick={() => setClicked((prevSt) => !prevSt)}
-        >
-          {clicked ? (
-            <ion-icon name="heart" size="large"></ion-icon>
-          ) : (
-            <ion-icon name="heart-outline" size="large"></ion-icon>
-          )}
-        </div>
-        <img
-          src={mouseEnter ? product.imgH : product.img}
-          alt=""
-          className="mb-4 w-full cursor-pointer "
-          onMouseEnter={() => setMouseEnter(true)}
-          onMouseLeave={() => setMouseEnter(false)}
-        />
-        <p className="text-sm">{product.title}</p>
-        <p className="text-sm text-orange-500">{product.price}</p>
-        <p>{i++}</p>
-      </div>
-    </div>
-  );
-};
+import React from "react";
+import { deal, topRated } from "./Data";
+import Slider from "./Slider";
 
 const Deals = () => {
-  const [numOfClick, setNumOfClick] = useState(0);
-
-  const increaseTranslateXHandler = () => {
-    setNumOfClick((prevSt) => (prevSt = prevSt + 1));
-  };
-
-  const decreaseTranslateXHandler = () => {
-    setNumOfClick((prevSt) => (prevSt = prevSt - 1));
-  };
-  if (numOfClick > 0) setNumOfClick(0);
-  if (numOfClick < -5) setNumOfClick(-5);
-
   return (
-    <div className=" mx-auto mb-10 mt-[-15rem] max-w-[110rem]  bg-white">
-      <div className="mb-6 flex items-center justify-between border-b border-[#dcdcdc] p-4">
-        <h2 className="fontm text-xl">Deals of the Day</h2>
-        <button className="rounded border bg-blue-600 px-3 py-2 text-white transition-all hover:bg-white hover:text-primary">
-          View all
-        </button>
+    <>
+      <div className=" mt-[-15rem]">
+        <Slider data={deal} />
       </div>
-
-      <div className="group relative w-full ">
-        <div
-          className="absolute left-0 top-[50%] z-10 hidden translate-y-[-50%] cursor-pointer items-center justify-center rounded-r-lg bg-backg px-2 py-7 shadow-lg group-hover:flex"
-          onClick={increaseTranslateXHandler}
-        >
-          <ion-icon name="chevron-back-outline" size="large"></ion-icon>
-        </div>
-
-        <div
-          className="absolute -right-1 top-[50%] z-10 hidden translate-y-[-50%] cursor-pointer items-center justify-center rounded-r-lg bg-backg px-2 py-7 shadow-lg group-hover:flex"
-          onClick={decreaseTranslateXHandler}
-        >
-          <ion-icon name="chevron-forward-outline" size="large"></ion-icon>
-        </div>
-
-        <div className="sec relative mb-10  h-[23rem] overflow-scroll scroll-smooth  ">
-          {deal.map((product, i) => (
-            <SingleProduct
-              product={product}
-              key={i}
-              i={i}
-              numOfClick={numOfClick}
-            />
-          ))}
-        </div>
-      </div>
-    </div>
+      <Slider data={topRated} />
+      <Slider data={topRated} />
+    </>
   );
 };
 
